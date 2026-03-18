95a0cf6571e649c54d290f9adbd30277f2a122c9
Siddharth172004 <siddharthdhole60@gmail.com>
2026-03-18 20:21:38 +0530

    Update chunks and emebedding

--- chunking.py
+++ chunking.py
@@ -9,73 +9,29 @@ import os
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
+#load_Data
 doc = fitz.open("final_dataset.pdf")
 
-text_data = []
+documents = []
 
 for page in doc:
     text = page.get_text()
-    text_data.append(text)
+    documents.append(text)
 
+#text chunking
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
     chunk_overlap = 200
 )
 
-chunks = text_splitter.split_text("\n".join(text_data))
-documents = [Document(page_content=chunk) for chunk in chunks]
+text_docs = []
+for i,text_page in enumerate(documents):
+    chunks = text_splitter.split_text(text_page)
+    for chunk in chunks:
+        d = Document(page_content= chunk,metadata={"page" : i,"source" : "text"})
+        text_docs.append(d)
 
-model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-embed = FAISS.from_documents(documents,model)
-embed.save_local("Vectordb/text_index")
-
-# os.makedirs("images",exist_ok = True)
-
-# image_paths = []
-
-# for page_index in range(len(doc)):
-
-#     page = doc[page_index]
-
-#     images = page.get_images(full=True)
-
-#     for img_index, img in enumerate(images):
-
-#         xref = img[0]
-
-#         base_image = doc.extract_image(xref)
-
-#         image_bytes = base_image["image"]
-#         # image size check
-#         width = base_image["width"]
-#         height = base_image["height"]
-
-#         # small images skip
-#         if width < 400 or height < 400:
-#             continue
-
-#         image_path = f"images/page{page_index}_{img_index}.png"
-
-#         with open(image_path, "wb") as f:
-#             f.write(image_bytes)
-
-#         image_paths.append(image_path)
-
-# ocr_texts = []
-
-# for path in image_paths:
-
-#     image = Image.open(path)
-
-#     text = pytesseract.image_to_string(image)
-
-#     # whitespace remove
-#     text = text.strip()
-
-#     # meaningful text check
-#     if len(text) > 20:
-#         ocr_texts.append(text)
-
+#Implement OCR (Extract text from images)
 ocr_texts = []
 
 for page_num in range(len(doc)):
@@ -86,9 +42,16 @@ for page_num in range(len(doc)):
 
     if text.strip():
         ocr_texts.append(text)
+        
+image_docs = []
+for i,image_page in enumerate(ocr_texts):
+    chunks = text_splitter.split_text(image_page)
+    for chunk in chunks:
+        i = Document(page_content=chunk ,metadata= {"page":i,"source":"image"}) 
+        image_docs.append(i)
 
-image_docs = [Document(page_content=t) for t in ocr_texts]
-
-image_vectorstore = FAISS.from_documents(image_docs, model)
-
-image_vectorstore.save_local("Vectordb/image_index")
+#Genrate Embedding
+model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+all_docs = text_docs + image_docs
+vectorstore = FAISS.from_documents(all_docs, model)
+vectorstore.save_local("Vectordb")
